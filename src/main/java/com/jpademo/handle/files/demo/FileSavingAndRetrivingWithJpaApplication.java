421932aac285adf2780cca7ac2aa27646a091308
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-29 23:27:11 +0530

    initial commit

---
+++ src/main/java/com/jpademo/handle/files/demo/FileSavingAndRetrivingWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.handle.files.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSavingAndRetrivingWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSavingAndRetrivingWithJpaApplication.class, args);
+	}
+
+}
